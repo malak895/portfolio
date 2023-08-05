4038a2871cbf12a2b2bdb339325d9c21e30769ff
malak895 <malak.hammami263@gmail.com>
2023-08-05 13:07:00 +0100

    first commit

--- pages/index.js
+++ pages/index.js
@@ -80,7 +80,8 @@ export default function Home() {
               I&apos;m a full stack developer.
             </h3>
             <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">
-              Hi, I&apos;m a full stack developer with experience in TypeScript and JavaScript.I have expertise in frameworks such as Angular, React, and Node.js. 
+              Hi, I&apos;m a full stack developer with experience in TypeScript and JavaScript.
+              I have expertise in frameworks such as Angular, React, and Node.js. 
               I&apos;m a fast learner and enjoy working closely with clients to develop efficient, scalable, and user-friendly solutions that address real-world problems.
             </p>
             <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
